2329ac381ca16d6efe4a507010cbbd84df344335
Daniel17971 <danielisa1922@gmail.com>
2023-05-02 13:44:44 +0100

    map centers on user location

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,12 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import { PROVIDER_GOOGLE } from "react-native-maps";
-
+import * as Location from "expo-location";
 function Map() {
+  const onRegionChange = (region) => {};
+  const [location, setLocation] = useState(null);
+  const { width, height } = Dimensions.get("window");
+  const aspectRatio = width / height;
+  const latitudeDelta = 0.02;
+  const longitudeDelta = latitudeDelta * aspectRatio;
+
+  const [initialPosition, setInitialPosition] = useState(null);
+  useEffect(() => {
+    Location.requestForegroundPermissionsAsync()
+      .then(({ status }) => {
+        if (status !== "granted") {
+          console.log("please grant permission");
+          return;
+        }
+      })
+      .then(() => {
+        return Location.getCurrentPositionAsync();
+      })
+      .then((data) => {
+        setLocation(data);
+        return data;
+      })
+      .then((data) => {
+        return setInitialPosition((currentPostition) => {
+          return {
+            latitudeDelta,
+            longitudeDelta,
+            latitude: data.coords.latitude,
+            longitude: data.coords.longitude,
+          };
+        });
+      });
+  }, [setLocation, setInitialPosition]);
+
   return (
     <View style={styles.container}>
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map} />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={initialPosition}
+        onRegionChange={onRegionChange}
+      />
     </View>
   );
 }
