d14acebe59182d9c0e3cd3be181fc4ec4873b4a4
Daniel17971 <109083455+Daniel17971@users.noreply.github.com>
2023-05-04 15:56:20 +0100

    added basic userpage layout and map edits

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-
+import Ionicons from "react-native-vector-icons/Ionicons";
 import Home from "./components/Home";
 import Map from "./components/Map";
 import User from "./components/User";
@@ -13,9 +13,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home";
+            } else if (route.name === "Map") {
+              iconName = focused ? "map" : "map";
+            } else if (route.name === "User") {
+              iconName = focused ? "person" : "person";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Home" component={Home} options={{ title: "Home" }} />
-        <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Map" component={Map} options={{ title: "Map" }} />
         <Tab.Screen name="User" component={User} options={{ title: "User" }} />
       </Tab.Navigator>
     </NavigationContainer>
