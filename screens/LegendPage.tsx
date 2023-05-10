ccfa96539a92796bb85a501f30c43fb090c5e653
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-05-10 16:42:04 +0100

    add styling to single legend page

--- screens/LegendPage.tsx
+++ screens/LegendPage.tsx
@@ -4,7 +4,7 @@ const LegendPage = ({ navigation, route }) => {
   const { legend } = route.params;
   console.log(legend, "<-- legend");
   return (
-    <View>
+    <View style={styles.container}>
       <Button
         title="back"
         onPress={() => {
@@ -12,8 +12,8 @@ const LegendPage = ({ navigation, route }) => {
         }}
       />
       <Text style={styles.title}>{legend.title}</Text>
-      <Text style={styles.subtitle}>{legend.legendCategory}</Text>
-      <Text style={styles.subtitle}>{legend.description}</Text>
+      <Text style={styles.category}>{legend.legendCategory}</Text>
+      <Text style={styles.description}>{legend.description}</Text>
     </View>
   );
 };
@@ -24,10 +24,34 @@ export default LegendPage;
 
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 20,
+  container: {
+    backgroundColor: "#2e2e9f",
+    height: "100%",
   },
-  subtitle: {
-    fontSize: 16
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    color: "#ffffff",
+    fontWeight: "bold",
+    marginVertical: 5,
+    marginHorizontal: 2.5,
+  },
+  category: {
+    fontSize: 16,
+    backgroundColor: '#ffe11b',
+    color: "#d9480f",
+    alignSelf: "center",
+    borderRadius: 6,
+    paddingVertical: 2,
+    paddingHorizontal: 10,
+    borderColor: "#ff7700",
+    textShadowColor: 'black',
+    marginBottom: 15,
+    marginTop: 4
+  },
+  description: {
+    color: "#ffffff",
+    fontSize: 18,
+    marginHorizontal: 15,
   }
 });
